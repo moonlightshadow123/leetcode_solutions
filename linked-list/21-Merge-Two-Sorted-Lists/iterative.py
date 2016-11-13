3c3e90db59f753d71d3e16fb07814248583d5b2c
moonlightshadow123 <2252822920@qq.com>
2016-11-13 15:43:04 +0800

    Create iterative.py

---
+++ linked-list/21-Merge-Two-Sorted-Lists/iterative.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        head = ListNode(0)
+        p = head
+        while l1 != None and l2 != None:
+            if l1.val < l2.val:
+                p.next = l1
+                l1 = l1.next
+            else:
+                p.next = l2
+                l2 = l2.next
+            p = p.next
+        if l1 == None:
+            p.next = l2
+        if l2 == None:
+            p.next = l1
+        return head.next
