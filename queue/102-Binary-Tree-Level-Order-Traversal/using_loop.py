41a2966f678a65a932585569739b2c99a936307c
moonlightshadow123 <2252822920@qq.com>
2016-11-13 17:20:12 +0800

    Create using_loop.py

---
+++ queue/102-Binary-Tree-Level-Order-Traversal/using_loop.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if root == None:
+            return []
+        queue = [root]
+        res = []
+        while len(queue) != 0:
+            curLevel = []
+            for i in range(len(queue)):
+                node = queue[0]
+                del queue[0]
+                curLevel.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(curLevel)
+        return res
